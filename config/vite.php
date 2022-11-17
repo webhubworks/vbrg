10e4e47d98a3e87915c908fe0ea85de44a2ff903
Niklas Sonnenschein <desk@niklassonnenschein.de>
2022-11-17 11:40:50 +0100

    Fix typecast issue

--- config/vite.php
+++ config/vite.php
@@ -3,11 +3,11 @@
 use craft\helpers\App;
 
 return [
-    'checkDevServer' => true,
+    'checkDevServer' => App::env('CRAFT_ENVIRONMENT') === 'dev',
     'devServerInternal' => 'http://localhost:3000',
     'devServerPublic' => Craft::getAlias('@web') . ':3000',
     'errorEntry' => 'src/scripts/app.js',
     'manifestPath' => Craft::getAlias('@webroot') . '/dist/manifest.json',
     'serverPublic' => Craft::getAlias('@web')  . '/dist/',
-    'useDevServer' => (bool) App::env('CRAFT_ENVIRONMENT') == 'dev',
+    'useDevServer' => App::env('CRAFT_ENVIRONMENT') === 'dev',
 ];
