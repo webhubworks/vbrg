23323e684daa740c07b5352c7766f3073612837d
Marten Brosch <brosch@webhub.de>
2023-12-01 10:09:47 +0100

    added twingle

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,27 +1,26 @@
-import { createApp } from 'vue';
-import AccordionItem from "@components/AccordionItem.vue";
-import Navigation from "@components/Navigation.vue";
-import NavigationButton from "@components/NavigationButton.vue";
-import Modal from "@components/Modal.vue";
-import Slider from "@components/Slider.vue";
-import KeyVisual from "@components/KeyVisual.vue";
-import CountUp from "@components/CountUp.vue";
-import {SwiperSlide} from 'swiper/vue';
+import AccordionItem from "@components/AccordionItem.vue"
+import CountUp from "@components/CountUp.vue"
+import KeyVisual from "@components/KeyVisual.vue"
+import Modal from "@components/Modal.vue"
+import Navigation from "@components/Navigation.vue"
+import NavigationButton from "@components/NavigationButton.vue"
+import Slider from "@components/Slider.vue"
+import { SwiperSlide } from "swiper/vue"
+import { createApp } from "vue"
 
-
-import '@styles/app.pcss';
+import "@styles/app.pcss"
 
 const app = createApp({
-    components: {
-        Navigation,
-        NavigationButton,
-        AccordionItem,
-        Slider,
-        SwiperSlide,
-        KeyVisual,
-        CountUp,
-        Modal
-    },
-});
+  components: {
+    Navigation,
+    NavigationButton,
+    AccordionItem,
+    Slider,
+    SwiperSlide,
+    KeyVisual,
+    CountUp,
+    Modal,
+  },
+})
 
-app.mount('#app');
+app.mount("#app")
