4793e2e6e38ca0cda08e6e8717ddbbb8b6984c21
Marten Brosch <brosch@webhub.de>
2023-12-04 14:10:00 +0100

    bottom matrix

--- src/scripts/components/Slider.vue
+++ src/scripts/components/Slider.vue
@@ -1,58 +1,68 @@
 <script setup>
-import {defineComponent} from 'vue';
-import {Navigation} from 'swiper/modules';
-import {Swiper, SwiperSlide} from 'swiper/vue';
+import { Navigation } from "swiper/modules"
+import { Swiper, SwiperSlide } from "swiper/vue"
+import { defineComponent } from "vue"
 
-import 'swiper/css';
+import "swiper/css"
 
 defineComponent({
   Swiper,
   SwiperSlide,
-});
+})
 
-const modules = [Navigation];
-
+const modules = [Navigation]
 </script>
 
 <template>
-  <div class="flex justify-center items-center space-x-8">
-
+  <div class="flex items-center justify-center space-x-8">
     <button
-        class="flex-shrink-0 gallery-prev bg-transparent text-yellow hover:bg-black hover:text-yellow h-16 w-16 lg:h-[80px] lg:w-[80px] flex items-center justify-center cursor:pointer rounded-full"
-        aria-controls="gallery"
-        aria-label="Vorheriges Element"
+      class="gallery-prev cursor:pointer flex h-16 w-16 flex-shrink-0 items-center justify-center rounded-full bg-transparent text-yellow hover:bg-black hover:text-yellow lg:h-[80px] lg:w-[80px]"
+      aria-label="Previous"
     >
-      <svg viewBox="0 0 52 45" class="h-10 lg:w-[40px] lg:h-auto fill-current" xmlns="http://www.w3.org/2000/svg">
-        <path d="M51.062 20.781H7.02L24.744 3.056 22.381.693.574 22.5l21.807 21.806 2.363-2.363L7.02 24.218h44.043z"
-              fill-rule="nonzero"/>
+      <svg
+        viewBox="0 0 52 45"
+        class="h-10 fill-current lg:h-auto lg:w-[40px]"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M51.062 20.781H7.02L24.744 3.056 22.381.693.574 22.5l21.807 21.806 2.363-2.363L7.02 24.218h44.043z"
+          fill-rule="nonzero"
+        />
       </svg>
+      <span class="sr-only">Previous</span>
     </button>
 
     <swiper
-        :slides-per-view="1"
-        class="mt-12"
-        :modules="modules"
-        :loop="true"
-        :navigation="{
+      :slides-per-view="1"
+      class="mt-12"
+      :modules="modules"
+      :loop="true"
+      :navigation="{
         nextEl: '.gallery-next',
         prevEl: '.gallery-prev',
         disabledClass: 'opacity-30',
       }"
-        slides-per-view="auto"
-        :space-between="30"
-        :centered-slides="true"
+      slides-per-view="auto"
+      :space-between="30"
+      :centered-slides="true"
     >
-     <slot></slot>
+      <slot></slot>
     </swiper>
     <button
-        class="flex-shrink-0 gallery-next bg-transparent text-yellow hover:bg-black hover:text-yellow h-16 w-16 lg:h-[80px] lg:w-[80px] flex items-center justify-center cursor:pointer rounded-full"
-        aria-controls="gallery"
-        aria-label="Nächstes Element"
+      class="gallery-next cursor:pointer flex h-16 w-16 flex-shrink-0 items-center justify-center rounded-full bg-transparent text-yellow hover:bg-black hover:text-yellow lg:h-[80px] lg:w-[80px]"
+      aria-label="Next"
     >
-      <svg viewBox="0 0 52 45" class="h-10 lg:w-[40px] lg:h-auto fill-current" xmlns="http://www.w3.org/2000/svg">
-        <path d="m29.62.693-2.364 2.363L44.98 20.781H.938v3.437H44.98L27.256 41.943l2.363 2.363L51.425 22.5z"
-              fill-rule="nonzero"/>
+      <svg
+        viewBox="0 0 52 45"
+        class="h-10 fill-current lg:h-auto lg:w-[40px]"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="m29.62.693-2.364 2.363L44.98 20.781H.938v3.437H44.98L27.256 41.943l2.363 2.363L51.425 22.5z"
+          fill-rule="nonzero"
+        />
       </svg>
+      <span class="sr-only">Next</span>
     </button>
   </div>
 </template>
